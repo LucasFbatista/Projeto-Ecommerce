1a904f63ae63bcf47976097cb38449b6eaf546d9
Lucas Ferreira <lucasferreirabatista5@gmail.com>
2019-06-11 22:45:15 -0300

    Deployer Teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Hello Word !
+                    Laravel New Word !
                 </div>
 
                 <div class="links">
